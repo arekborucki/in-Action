d7191c759ba5ecfa9a0b6d47a19ce24908b6209d
Arek Borucki <68437078+arekborucki@users.noreply.github.com>
2024-06-03 16:41:34 +0200

    Create mongodb-pymongo.py

---
+++ Chapter-3/mongodb-pymongo.py
@@ -0,0 +1,30 @@
+# Using PyMongo (Python Driver)
+# Install the PyMongo driver
+python3 -m pip install pymongo
+
+# Example PyMongo script to connect and query MongoDB
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pprint import pprint
+
+uri = "<connection_string>"
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+database = client['sample_training']
+routes_collection = database['routes']
+query = {"src_airport": "JFK", "airline.id": 3201}
+
+try:
+  route = routes_collection.find_one(query)
+  if route:
+    print("Found a route:")
+    pprint(route)
+  else:
+    print("No route found from JFK.")
+except Exception as e:
+  print(f"An error occurred: {e}")
+finally:
+  client.close()
+
+# Execute the PyMongo script
+python3 mongodb-pymongo.py
